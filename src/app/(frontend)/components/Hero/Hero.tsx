8a226662fb68b2742f51cd29fe834ebae08fbbf9
Wichosu <luiscalderonmiranda@gmail.com>
2026-01-11 11:44:32 -0600

    Minimal hero

--- src/app/(frontend)/components/Hero/Hero.tsx
+++ src/app/(frontend)/components/Hero/Hero.tsx
@@ -1,14 +1,62 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export function Hero() {
   return (
-    <section className="flex">
-      <div>
-        <h1>Hi, I&apos;m Wichosu</h1>
-        <p>I&apos;m a web developer and build apps for the web and soon mobile</p>
+    <section className="flex justify-evenly">
+      <div className="max-w-2xl flex flex-col gap-4">
+        <h1 className="text-4xl font-medium">Hi, I&apos;m Wichosu</h1>
+        <p className="text-xl">
+          I&apos;m a web developer and I build apps for the web and soon mobile.
+        </p>
+        <p className="text-xl">
+          Everything I build is public and open source in my github profile.
+        </p>
+        <p className="text-xl">
+          If you want to build something and need advice, feel free to contact me and we can have a
+          casual chat. I don&apos;t charge for advice.
+        </p>
+        <p className="text-xl">
+          You can find me on{' '}
+          <Link
+            className="text-blue-500 hover:text-blue-600"
+            href="https://github.com/wichosu"
+            target="_blank"
+          >
+            GitHub
+          </Link>
+          ,{' '}
+          <Link
+            className="text-blue-500 hover:text-blue-600"
+            href="https://Linkter.com/wichosu"
+            target="_blank"
+          >
+            Twitter
+          </Link>
+          ,{' '}
+          <Link
+            className="text-blue-500 hover:text-blue-600"
+            href="https://linkedin.com/in/wichosu"
+            target="_blank"
+          >
+            LinkedIn
+          </Link>
+          ,{' '}
+          <Link
+            className="text-blue-500 hover:text-blue-600"
+            href="https://t.me/wichosu"
+            target="_blank"
+          >
+            Telegram
+          </Link>
+          .
+        </p>
+        <p className="text-xl">
+          I take time to respond to messages, but I&apos;ll get back to you as soon as I can.
+        </p>
       </div>
-      <picture>
+      <picture className="rounded-lg overflow-hidden">
         <Image src="/wichosu.jpeg" alt="Wichosu" width={325} height={325} />
       </picture>
     </section>
