fe3e1009f38b048c6671a3d64152339374684450
Wichosu <luiscalderonmiranda@gmail.com>
2026-01-11 21:13:23 -0600

    minimal website done

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -4,8 +4,13 @@ import { Container } from './components/Container'
 // import { Navbar } from './components/Navbar'
 
 export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
+  description:
+    "Hi, I'm Wichosu, I'm a web developer and I build open source projects. This is my personal website. Feel free to contact me if you need advice or want to build something.",
+  title: 'Wichosu - Web Developer',
+  twitter: {
+    card: 'summary_large_image',
+  },
+  metadataBase: new URL('https://www.wichosu.com/'),
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
