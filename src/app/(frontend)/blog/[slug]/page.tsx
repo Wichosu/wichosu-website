94ea6854724de1e0ed2ff8f6a365fd0a2b173905
Wichosu <luiscalderonmiranda@gmail.com>
2026-01-15 20:36:34 -0600

    blog entry fetch, missing styles

--- src/app/(frontend)/blog/[slug]/page.tsx
+++ src/app/(frontend)/blog/[slug]/page.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { getPayload } from 'payload'
+import config from '@payload-config'
+import { RichText } from '@payloadcms/richtext-lexical/react'
 
 type Props = {
   params: {
@@ -6,6 +9,28 @@ type Props = {
   }
 }
 
-export default function Page({ params }: Props) {
-  return <div>Page {params.slug}</div>
+export default async function Page({ params }: Props) {
+  const payload = await getPayload({ config })
+
+  const blogEntry = await payload.find({
+    collection: 'blog-entry',
+    where: {
+      slug: {
+        equals: params.slug,
+      },
+    },
+    limit: 1,
+  })
+
+  if (!blogEntry.docs[0]) {
+    return <div>Blog entry not found</div>
+  }
+
+  return (
+    <>
+      <h1>{blogEntry.docs[0].title}</h1>
+      <p>{new Date(blogEntry.docs[0].date).toLocaleDateString()}</p>
+      <RichText data={blogEntry.docs[0].content} />
+    </>
+  )
 }
